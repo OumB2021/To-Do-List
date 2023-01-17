9871e0f17cb58afea21f7dc1a3653a8dca788334
Oumar <baarryoumar@gmail.com>
2023-01-17 17:32:28 -0500

    creating another page

--- app.js
+++ app.js
@@ -7,28 +7,40 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static("public"));
 
-var items = ["Buy Food", "Cook Food", "Workout"];
+let items = ["Buy Food", "Cook Food", "Workout"];
+let workItems = [];
+
 
 app.get('/', function(req, res){
 
-  var today = new Date();
+  let today = new Date();
   
-  var options = {
+  let options = {
     weekday:"long",
     day:"numeric",
     month:"long"
   }
 
-  var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay : day, newListItems : items});
+  let day = today.toLocaleDateString("en-US", options);
+  res.render("list", {listTitle : day, newListItems : items});
+});
+
+app.get('/work', function(req, res){
+  res.render('list', {listTitle : "workList", newListItems : workItems});
 });
 
 app.post('/', function(req, res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
 
   res.redirect('/');
 });
+
+app.post('/work', function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
+})
 
 app.listen(3000, function(){
   console.log('listening on 3000');
