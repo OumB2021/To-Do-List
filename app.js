efd8b92c42df9ebbde5561a66776f8aa85061c64
Oumar <baarryoumar@gmail.com>
2023-01-17 16:06:01 -0500

    list of items created

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended:true }));
 
+var items = [];
+
 app.get('/', function(req, res){
 
   var today = new Date();
@@ -17,8 +19,14 @@ app.get('/', function(req, res){
   }
 
   var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {kindOfDay : day, newListItems : items});
+});
 
-  res.render("list", {kindOfDay : day});
+app.post('/', function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+
+  res.redirect('/');
 });
 
 app.listen(3000, function(){
